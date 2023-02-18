816b5aea1847dc098cbb214837f0ba822bddfd6b
hhan0015 <hhan0015@student.monash.edu>
2023-02-18 20:54:03 +0800

    1

---
+++ studyProject/spReview/src/test/java/com/example/spreview/SpReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
